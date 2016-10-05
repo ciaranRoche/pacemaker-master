e65cc6546a2e3105799e982c2251d8f240fbc4b4
ciaran <ciaranroche@hotmail.co.uk>
2016-10-05 10:07:49 +0100

    pacemakerAPItest reconfigured

--- test/models/PacemakerAPITest.java
+++ test/models/PacemakerAPITest.java
@@ -17,6 +17,9 @@ public class PacemakerAPITest {
 	@Before
 	public void setup(){
 		pacemaker = new PacemakerAPI();
+		for (User user : users){
+			pacemaker.createUser(user.firstName, user.lastName, user.email, user.password);
+		}
 	}
 	
 	@After
@@ -29,16 +32,15 @@ public class PacemakerAPITest {
 		assertEquals(0, pacemaker.getUsers().size());
 	}**/
 	
-	/**@Test 
+	@Test 
 	public void testUser(){
-		User homer = new User("homer", "simpson", "homer@simpson.com", "secret");
-		
-		assertEquals(0, pacemaker.getUsers().size());
-		pacemaker.createUser("homer", "simpson", "homer@simpson.com", "secret");
-		assertEquals(1, pacemaker.getUserByEmail("homer@simpson.com"));
-	}**/
+		assertEquals (users.length, pacemaker.getUsers().size());
+	    pacemaker.createUser("homer", "simpson", "homer@simpson.com", "secret");
+	    assertEquals (users.length+1, pacemaker.getUsers().size());
+	    assertEquals (users[0], pacemaker.getUserByEmail(users[0].email));
+	}
 	
-	@Test
+/**	@Test
 	public void testEquals(){
 		User homer = new User("homer", "simpson", "homer@simpson.com", "secret");
 		 User homer2 = new User ("homer", "simpson", "homer@simpson.com",  "secret"); 
@@ -47,19 +49,24 @@ public class PacemakerAPITest {
 		 assertEquals(homer, homer);
 		 assertEquals(homer, homer2);
 		 assertNotEquals(homer, bart);
-	}
+	}**/
 	
 	@Test
 	public void testUsers(){
-		for (User user : users){
-			pacemaker.createUser(user.firstName, user.lastName, user.email, user.password);
-		}
 		assertEquals(users.length, pacemaker.getUsers().size());
-		for(User user : users){
+		for (User user : users){
 			User eachUser = pacemaker.getUserByEmail(user.email);
-			assertEquals (user, eachUser);
+			assertEquals(user, eachUser);
 			assertNotSame(user, eachUser);
 		}
+	}
+	
+	@Test
+	public void testDeleteUsers(){
+		assertEquals(users.length, pacemaker.getUsers().size());
+		User marge = pacemaker.getUserByEmail("marge@simpson.com");
+		pacemaker.deleteUser(marge.id);
+		assertEquals(users.length-1, pacemaker.getUsers().size());
 	}
 	
 }
